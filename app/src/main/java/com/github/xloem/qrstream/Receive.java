a3cee0c47f576557284070d0fbb7b4ffdf9891e6
xloem <0xloem@gmail.com>
2015-05-01 20:23:24 -0400

    Created Receive Activity to concatenate QR codes to a file.

---
+++ app/src/main/java/com/github/xloem/qrstream/Receive.java
@@ -0,0 +1,110 @@
+package com.github.xloem.qrstream;
+
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+public class Receive extends Activity {
+
+    private int index;
+    private byte[] lastBytes;
+
+    private File tempFile;
+    private BufferedWriter tempWriter;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("index", index);
+        outState.putByteArray("lastBytes", lastBytes);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        tempFile = new File(getApplicationContext().getExternalCacheDir(), "qrstream.txt");
+
+        if (savedInstanceState == null) {
+            index = 1;
+            lastBytes = null;
+            tempFile.delete();
+        } else {
+            index = savedInstanceState.getInt("index");
+            lastBytes = savedInstanceState.getByteArray("lastBytes");
+        }
+
+        try {
+            tempWriter = new BufferedWriter(new FileWriter(tempFile, true));
+        } catch( IOException e) {
+            Toast.makeText(getApplicationContext(), "ERROR: Failed to open " + tempFile.getPath(), Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+            setResult(RESULT_CANCELED, getIntent());
+            finish();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        readOne();
+    }
+
+    private void readOne() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        // TODO: handle if zxing is not installed (AlertDialog is returned rather than null)
+        integrator.addExtra("RESULT_DISPLAY_DURATION_MS", 0L);
+        integrator.addExtra("PROMPT_MESSAGE", "Scan QR Code #" + String.valueOf(index) + " or hit back if done.");
+        integrator.initiateScan(IntentIntegrator.QR_CODE_TYPES);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (result != null) try {
+            if (result.getFormatName() != null) {
+                byte[] bytes = result.getRawBytes();
+                if (!Arrays.equals(bytes, lastBytes)) {
+                    // not a rescan of last qr
+                    tempWriter.write(result.getContents());
+                    index++;
+                    lastBytes = bytes;
+                }
+                // readOne() will be called in onResume
+                return;
+            }
+
+            // zxing returned without result, done
+
+            tempWriter.close();
+
+            if (tempFile.length() > 0) {
+                Intent aIntent = getIntent();
+                aIntent.setData(Uri.fromFile(tempFile));
+                setResult(RESULT_OK, aIntent);
+            } else {
+                setResult(RESULT_CANCELED, getIntent());
+            }
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "ERROR: Failed to write to " + tempFile.getPath(), Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+
+            setResult(RESULT_CANCELED, getIntent());
+        }
+
+        finish();
+    }
+}
---
+++ app/src/main/java/com/github/xloem/qrstream/Send.java
@@ -0,0 +1,13 @@
+package com.github.xloem.qrstream;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Send extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
