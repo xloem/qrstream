6cf5ffcc93c2aa08c0cd3ab1638b2f6d54a6c04d
xloem <0xloem@gmail.com>
2015-07-23 16:58:18 -0400

    Implement send using zxing core rather than intents, allowing for custom display of larger & faster codes.

--- android/app/src/main/java/com/github/xloem/qrstream/Send.java
+++ android/app/src/main/java/com/github/xloem/qrstream/Send.java
@@ -1,18 +1,24 @@
 package com.github.xloem.qrstream;
 
-import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Display;
+import android.view.KeyEvent;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
@@ -22,6 +28,8 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.nio.CharBuffer;
+import java.util.EnumMap;
+import java.util.Map;
 
 
 public class Send extends Activity {
@@ -35,9 +43,10 @@ public class Send extends Activity {
     private int index;
     private int total;
 
-    private String codeFormat;
-
-    private boolean waiting;
+    private BarcodeFormat codeFormat;
+    Map<EncodeHintType,Object> hints;
+    private int displaySize;
+    private Bitmap bitmap;
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -52,29 +61,44 @@ public class Send extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send);
 
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
 
-        codeFormat = sharedPref.getString("code_format", "QR_CODE");
+        codeFormat = BarcodeFormat.valueOf(sharedPref.getString("code_format", "QR_CODE"));
+
+        hints = new EnumMap<EncodeHintType,Object>(EncodeHintType.class);
+        hints.put(EncodeHintType.CHARACTER_SET, "ISO-8859-1");
+
+        LinearLayout rootLayout = (LinearLayout) findViewById(R.id.root_layout);
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int displayWidth = display.getWidth();
+        int displayHeight = display.getHeight();
+        float displayInches;
+        if (displayWidth < displayHeight) {
+            displayInches = displayWidth / metrics.xdpi;
+            displaySize = displayWidth;
+            rootLayout.setOrientation(LinearLayout.VERTICAL);
+        } else {
+            displayInches = displayHeight / metrics.ydpi;
+            displaySize = displayHeight;
+            rootLayout.setOrientation(LinearLayout.HORIZONTAL);
+        }
+        bitmap = Bitmap.createBitmap(displaySize, displaySize, Bitmap.Config.ARGB_8888);
 
         // Guess the max barcode capacity given a target cell size
         float minCellMicrometers = Float.valueOf(sharedPref.getString("cell_size", "640"));
+        int codeSize = (int)(displayInches * 25400f / minCellMicrometers);
         int codeCapacity;
-
-        Display display = getWindowManager().getDefaultDisplay();
-        DisplayMetrics metrics = getResources().getDisplayMetrics();
-        float displayWidth = display.getWidth() / metrics.xdpi;
-        float displayHeight = display.getHeight() / metrics.ydpi;
-        float displayInches = displayWidth < displayHeight ? displayWidth : displayHeight;
-
-        displayInches = displayInches * 7 / 8;
-        int maxSize = (int)(displayInches * 25400f / minCellMicrometers);
-
-        if ("AZTEC".equals(codeFormat))
-            codeCapacity = Metrics.aztecCapacity(maxSize);
-        else
-            // zxing adds a 4 cell quiet zone on each side of a QR code
-            codeCapacity = Metrics.qrcodeCapacity(maxSize - 8);
+        if (codeFormat == BarcodeFormat.AZTEC) {
+            codeCapacity = Metrics.aztecCapacity(codeSize);
+        } else {
+            // we specify a 2-cell margin on all sides
+            // TODO: make this a preference
+            hints.put(EncodeHintType.MARGIN, 2);
+            codeCapacity = Metrics.qrcodeCapacity(codeSize - 4);
+        }
 
         int dataRemaining;
 
@@ -87,8 +111,7 @@ public class Send extends Activity {
                 uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
             } else {
                 Toast.makeText(getApplicationContext(), String.format(getString(R.string.except_unexpected_intent), getString(R.string.app_name) + " Send", intent.getAction()), Toast.LENGTH_LONG).show();
-                setResult(RESULT_CANCELED, getIntent());
-                finish();
+                cancel();
                 return;
             }
         } else {
@@ -128,81 +151,91 @@ public class Send extends Activity {
         // use actual average size given total number
         buffer = CharBuffer.allocate((dataRemaining - 1) / total + 1);
 
-        // Warn if there will be a lot of codes to display
-        if (offset == 0
-            && total > Integer.valueOf(sharedPref.getString("warning_threshold", "20")))
-        {
-            waiting = true;
+        total += index;
 
-            AlertDialog.Builder confirmationDialog = new AlertDialog.Builder(this);
-            confirmationDialog.setTitle(getString(R.string.send_confirm_title));
-            confirmationDialog.setMessage(String.format(getString(R.string.send_confirm_message), total));
-            confirmationDialog.setPositiveButton(getString(R.string.send_confirm_positive), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    waiting = false;
-                    writeOne();
-                }
-            });
-            confirmationDialog.setNegativeButton(getString(R.string.send_confirm_negative), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    cancel();
-                }
-
-            });
-            confirmationDialog.show();
-        } else {
-            waiting = false;
-
-            total += index;
-
-            if (offset == 0) {
-                Toast.makeText(getApplicationContext(), String.format(getString(R.string.send_sending_summary), dataRemaining, total), Toast.LENGTH_SHORT).show();
-            }
+        if (offset == 0) {
+            Toast.makeText(getApplicationContext(), String.format(getString(R.string.send_sending_summary), dataRemaining, total), Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (!waiting)
-            writeOne();
+        if (buffer.position() == 0)
+            readOne();
+        writeOne();
     }
+
+    @Override
+    public boolean onKeyDown(int code, KeyEvent event) {
+        if (super.onKeyDown(code, event))
+            return true;
+        if (code == KeyEvent.KEYCODE_CAMERA
+                || code == KeyEvent.KEYCODE_SEARCH
+                || code == KeyEvent.KEYCODE_CALL) {
+            readOne();
+            writeOne();
+            return true;
+        }
+        return false;
+    }
+
 
     private void cancel() {
         setResult(RESULT_CANCELED, getIntent());
         finish();
     }
 
-    private void writeOne() {
-        int len;
+    private void readOne() {
         try {
             buffer.rewind();
             buffer.limit(buffer.capacity());
-            len = dataReader.read(buffer);
+            int len = dataReader.read(buffer);
+            if (len <= 0) {
+                setResult(RESULT_OK, getIntent());
+                finish();
+            }
         } catch (IOException e) {
             Toast.makeText(getApplicationContext(), String.format(getString(R.string.except_io), e.getMessage(), ""), Toast.LENGTH_LONG).show();
             e.printStackTrace();
             cancel();
+        }
+    }
+
+    private void writeOne() {
+        int len = buffer.position();
+        if (len == 0)
+            return;
+        index ++;
+        offset += len;
+
+        buffer.rewind();
+        buffer.limit(len);
+        BitMatrix bitmatrix;
+        try {
+            bitmatrix = new MultiFormatWriter().encode(buffer.toString(), codeFormat, displaySize, displaySize, hints);
+        } catch (WriterException e) {
+            Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+            cancel();
             return;
         }
-        if (len > 0) {
-            index ++;
-            offset += len;
 
-            IntentIntegrator integrator = new IntentIntegrator(this);
-            integrator.addExtra("ENCODE_SHOW_CONTENTS", false);
-            buffer.rewind();
-            buffer.limit(len);
-            integrator.addExtra("ENCODE_FORMAT", codeFormat);
-            if (integrator.shareText(buffer) != null)
-                // zxing not installed
-                cancel();
-        } else {
-            setResult(RESULT_OK, getIntent());
-            finish();
+        int[] pixels = new int[displaySize * displaySize];
+        int pixelIndex = 0;
+        for (int y = 0; y < displaySize; y++) {
+            for (int x = 0; x < displaySize; x++) {
+                pixels[pixelIndex ++] = bitmatrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF;
+            }
         }
+
+        bitmap.setPixels(pixels, 0, displaySize, 0, 0, displaySize, displaySize);
+
+        ImageView view = (ImageView) findViewById(R.id.image_view);
+        view.setImageBitmap(bitmap);
+
+        TextView codeLabel = (TextView) findViewById(R.id.code_label);
+        codeLabel.setText(String.format(getString(R.string.send_code_label), index, total, len, offset - len));
     }
 
 }
